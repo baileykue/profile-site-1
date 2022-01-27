37be0a9c196b0d203030c31c101da72d69dbc7c7
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-26 19:01:35 -0800

    added basic styling to header and footer elements

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,12 @@
 import './Footer.css';
 
+//have links to social media, github, and linked in
+
 export default function Footer() {
   return (
-    <div>
-      <p>footer</p>
+    <div className="footer">
+      <a href="https://github.com/baileykue">github</a>
+      <a href="https://www.linkedin.com/in/bailey-kuebelbeck/">linked in</a>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
+
+//simple navigation bar to jump down to projects, resume, or contact info
 
 export default function Header() {
   return (
-    <div>
-      <p>header</p>
+    <div className="header">
+      <p>profile</p>
+      <p>projects</p>
+      <p>resume</p>
     </div>
   );
 }
